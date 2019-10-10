63e801c89f0d14291f11aadda5020bdce174d1d6
ChClaude <christ.tchambila@gmail.com>
2019-10-10 20:44:35 +0200

    layout updated

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,49 +2,96 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class Controller {
+public class Controller implements Initializable {
+
+    @FXML
+    private Button btnNewCustomer;
+
+    @FXML
+    private Button btnNewVehicle;
+
+    @FXML
+    private Button btnRentVehicle;
+
+    @FXML
+    private Button btnReturnVehicle;
+
+    @FXML
+    private Button btnListAllRentals;
+
+    @FXML
+    private Pane status;
+
+    @FXML
+    private Label labelStatus;
+
+    @FXML
+    private GridPane pnAddCustomers;
+
+    @FXML
+    private GridPane pnAllRentals;
+
+    @FXML
+    private GridPane pnAddVehicle;
+
+    @FXML
+    private GridPane pnRentVehicle;
+
 
     public Controller() {
 //        Client client = new Client("localhost", 8085);
+    }
 
-
-   }
-
-   @FXML
-   public void addNewCustomer(ActionEvent e) {
-       System.out.println("Add a new customer");
-   }
-
-    @FXML
-    public void addNewVehicle(ActionEvent e) {
-        System.out.println("Add a new vehicle");
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // the is the initializing method
     }
 
     @FXML
-    public void rentVehicle(ActionEvent e) {
-        System.out.println("rent a vehicle");
-    }
+    private void handleClicks(ActionEvent event) {
 
-    @FXML
-    public void returnVehicle(ActionEvent e) {
-        System.out.println("return a vehicle");
-    }
+        if (event.getSource() == btnNewCustomer) {
+            // New Customer
+            labelStatus.setText("Customers");
+            status.setBackground(new Background(new BackgroundFill(Color.rgb(113, 86, 221),
+                    CornerRadii.EMPTY, Insets.EMPTY)));
+            pnAddCustomers.toFront();
+        } else if (event.getSource() == btnNewVehicle) {
+            // New Vehicle
+            labelStatus.setText("Vehicles");
+            status.setBackground(new Background(new BackgroundFill(Color.rgb(43, 63, 99),
+                    CornerRadii.EMPTY, Insets.EMPTY)));
+            pnAddVehicle.toFront();
+        } else if (event.getSource() == btnRentVehicle) {
+            // Rent Vehicle
+            labelStatus.setText("Rentals");
+            status.setBackground(new Background(new BackgroundFill(Color.rgb(44, 99, 63),
+                    CornerRadii.EMPTY, Insets.EMPTY)));
+            pnRentVehicle.toFront();
+        } else if (event.getSource() == btnReturnVehicle) {
+            // Return Vehicle
+            labelStatus.setText("Returns");
+            status.setBackground(new Background(new BackgroundFill(Color.rgb(99, 43, 63),
+                    CornerRadii.EMPTY, Insets.EMPTY)));
+        } else if (event.getSource() == btnListAllRentals) {
+            // List rentals
+            labelStatus.setText("All Rentals");
+            status.setBackground(new Background(new BackgroundFill(Color.rgb(42, 28, 66),
+                    CornerRadii.EMPTY, Insets.EMPTY)));
+            pnAllRentals.toFront();
+        }
 
-    @FXML
-    public void listAllVehicles(ActionEvent e) {
-        System.out.println("list all vehicles");
-    }
-
-    @FXML
-    public void listAllCustomers(ActionEvent e) {
-        System.out.println("list all customers");
-    }
-
-    @FXML
-    public void displayAllRentals(ActionEvent e) {
-        System.out.println("Display all rentals");
     }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,20 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Car Rentals");
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Car Rentals");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
